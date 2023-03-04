d592ee74069e82a6aea229fe0fc44a19ab7be972
Yo-mah-Ya <shanzhongziye@gmail.com>
2023-03-03 17:31:26 -0800

    initial commit

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cmake --version
+if [ $? -eq 0 ] ; then
+    cd ~
+    wget https://downloads.apache.org//xerces/c/3/sources/xerces-c-3.2.3.tar.gz
+    tar xvzf xerces-c-3.2.3.tar.gz
+    cd xerces-c-3.2.3
+    ./configure && make && sudo make install
+
+    ldconfig
+fi
